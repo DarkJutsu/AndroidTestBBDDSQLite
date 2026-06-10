927b3da6b3b681b124fe1fecaec98ed8a52ec7d1
DarkJutsu <ulloajose34@gmail.com>
2026-06-10 12:05:10 -0600

    feat:integrate TaskViewModel into MainActivity and update UI with AppTask composable

--- app/src/main/java/com/samllo/testsqlite/MainActivity.kt
+++ app/src/main/java/com/samllo/testsqlite/MainActivity.kt
@@ -4,44 +4,48 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.samllo.testsqlite.ui.ScreenTask
+import com.samllo.testsqlite.ui.TaskViewModel
 import com.samllo.testsqlite.ui.theme.TestBBDDSQLiteTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            TestBBDDSQLiteTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+  val viewModel: TaskViewModel by viewModels()
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      TestBBDDSQLiteTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+          AppTask(
+            viewModel,
+            modifier = Modifier.padding(innerPadding)
+          )
         }
+      }
     }
+  }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppTask(viewModel: TaskViewModel, modifier: Modifier = Modifier) {
+  Surface(modifier = modifier.padding(horizontal = 16.dp)) {
+    ScreenTask(viewModel)
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TestBBDDSQLiteTheme {
-        Greeting("Android")
-    }
+fun AppTaskPreview() {
+  TestBBDDSQLiteTheme {
+//    AppTask()
+  }
 }
